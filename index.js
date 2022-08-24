0aa31325ddd7f255eb481425b64e8af1cd1c250c
Jeremy Tardini <jeremy.tardini@gmail.com>
2022-08-24 23:17:27 +0200

    affichage li

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 function createDiv(number) {
     if(document.getElementById("nombre")) {
         let old = document.getElementById("nombre")
-        div.removeChild(old)
+        NumberOfWorld.removeChild(old)
     }
     const newParagraphe = document.createElement("p")
+    newParagraphe.setAttribute('id', "nombre")
     newParagraphe.append(`Il y a ${number} mots`)
     return newParagraphe
     
+}
+
+function afficheListWord() {
+if(document.getElementById("ul")) {
+    const ul = document.getElementById('ul')
+    ul.parentNode.removeChild(ul)
+}
+const ul = document.createElement("ul")
+ul.setAttribute("id", "ul")
+for(const i in content) {
+    let li = document.createElement("li")
+    li.append(`${content[i].word} : ${content[i].nbr} fois`)
+    console.log(content[i])
+    ul.appendChild(li)
+    listWord.appendChild(ul)
+}
 }
 
 function compte() {
     const text = document.getElementById("Compteur").value
     let nombre = 0
     const array = text.split(" ")
-    howMany(array)
+    for(const i in array) {
+        console.log(array[i])
+        array.splice("\n", 1)
+    }
+    howManyDistinctWord(array)
     console.log(array)
+    console.log(content)
     const espace = text.split("\n")
     for(const i in array){
         if(array[i] != "") {
@@ -27,20 +49,21 @@ function compte() {
         }
     }
     const p = createDiv(nombre)
-    div.appendChild(p)
+    NumberOfWorld.appendChild(p)
+    afficheListWord()
 }
 
-function howMany(Array) {
-    let content = []
-    if(!content.length) {
-        console.log("creation")
-        let word = {
-            word: Array[0],
-            nbr: 1
-        }
-        content.push(word)
-        console.log(content)
+function howManyDistinctWord(Array) {
+    if(content.length) {
+        content.length = 0
     }
+    console.log("creation")
+    let word = {
+        word: Array[0],
+        nbr: 1
+    }
+    content.push(word)
+    console.log(content)
     for(let i = 1; i < Array.length; i++) {
         tchekWords(Array[i], content)
     }
@@ -63,10 +86,14 @@ function tchekWords(currentWord, content) {
                 nbr: 1
             }
             content.push(newWord)
+            return
         }
     }
 }
 
+let content = []
 const button = document.getElementById('compte')
 button.addEventListener('click', compte)
-const div = document.getElementById("NbrMots")
+const NumberOfWorld = document.getElementById("NbrMots")
+const listWord = document.getElementById("ListeWords")
+
